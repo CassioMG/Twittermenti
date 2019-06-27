8ed69a323edeb35db6bab13420c9aca6b10fc9e9
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2019-06-27 16:36:48 -0300

    Customize screen animation based on current system's keyboard size.

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -8,14 +8,20 @@
 
 import UIKit
 import SwifteriOS
+// TODO: use SVProgressHUD to give user feedback
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController {
     
     // TODO: set your consumerKey and consumerSecret here:
     private let swifter = Swifter(consumerKey: "", consumerSecret: "")
-    
     private let kTweetsCount = 100
     private let kTweetsLanguage = "en"
+    
+    // Default keyboard animation values (iPhone 8 Plus)
+    private var keyboardHeight : CGFloat = 271.0
+    private var animationDuration : Double = 0.25
+    private var animationCurve : UInt = 7
+    private var initialBottomConstraintConstant: CGFloat = 0.0
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
@@ -24,8 +30,39 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(backgroundTapped))
+        backgroundView.addGestureRecognizer(tapGesture)
     }
 
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        initialBottomConstraintConstant = bottomConstraint.constant
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self)
+    }
+    
     
     @IBAction func predictPressed(_ sender: Any) {
         fetchTweets()
@@ -33,6 +70,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     
     private func fetchTweets () {
+        
+        if textField.text!.count == 0 {
+            return updateUI(with: 0)
+        }
         
         swifter.searchTweet(using: textField.text!, lang: kTweetsLanguage, count: kTweetsCount, tweetMode: .extended, success: { (jsonResults, jsonMetada) in
             
@@ -98,13 +139,44 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    
-    // MARK: Text Field delegate methods
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        UIView.animate(withDuration: 0.3) {
-            self.bottomConstraint.constant = 300.0
-            self.view.layoutIfNeeded()
-        }
+    // MARK: - Tap Gesture method
+    @objc private func backgroundTapped () {
+        textField.endEditing(true)
     }
+    
+    // MARK: - Keyboard Notifications
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            keyboardHeight = keyboardFrame.cgRectValue.height
+        }
+        
+        if let animationDurationNumber: NSNumber = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber {
+            animationDuration = animationDurationNumber.doubleValue
+        }
+        
+        if let animationCurveNumber: NSNumber = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? NSNumber {
+            animationCurve = animationCurveNumber.uintValue
+        }
+        
+        UIView.animate(withDuration: animationDuration, delay: 0, options: UIView.AnimationOptions(rawValue: animationCurve), animations: {
+            
+            self.bottomConstraint.constant = self.keyboardHeight + self.initialBottomConstraintConstant
+            self.view.layoutIfNeeded()
+            
+        }, completion: nil)
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        
+        UIView.animate(withDuration: animationDuration, delay: 0, options: UIView.AnimationOptions(rawValue: animationCurve), animations: {
+            
+            self.bottomConstraint.constant = self.initialBottomConstraintConstant
+            self.view.layoutIfNeeded()
+            
+        }, completion: nil)
+        
+    }
+    
     
 }
